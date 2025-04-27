5463596734148a0753c82a31e874f209ad331e28
Dinesh Kumar Sahoo <dineshsahoo702@gmail.com>
2025-04-28 00:36:28 +0530

    phone changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../style";
 import Computers from "../components/canvas/Computers";
-
+import laptop from "../assets/laptop1.jpg";
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -18,26 +35,33 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Dinesh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I build responsive and interactive web interfaces.
+            I build responsive and interactive web interfaces.
           </p>
         </div>
       </div>
-      {/* Desktop Image */}
-      <Computers />
-      {/* Scroling Element */}
+
+      {!isMobile ? (
+        <Computers />
+      ) : (
+        <img
+          src={laptop}
+          alt="3D Computer"
+          className="w-full h-full object-contain"
+          style={{ margin: "auto", width: "300px" }}
+        />
+      )}
+
       <div className="absolute flex justify-center items-center xs:bottom-[-10px] bottom-32 w-full">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev 
-            animate={{
-              y:[0,24,0]
-            }}
-            transition={{
-              duration:1.5,
-              repeat:Infinity,
-              repeatType:'loop'
-            }}
-            className="bg-secondary w-3 h-3 rounded-full mb-1"
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="bg-secondary w-3 h-3 rounded-full mb-1"
             />
           </div>
         </a>
